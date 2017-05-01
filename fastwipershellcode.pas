bf87f9afd5e8ba189aa925bda0c910c6d7c40f19
Evgeniy Popov <tuzik87@inbox.ru>
2017-05-01 23:42:34 +0600

    First stable release

---
+++ fastwipershellcode.pas
@@ -0,0 +1,145 @@
+unit fastwipershellcode;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, process, LCLType, UTF8Process, Forms, Controls,
+  Graphics, ExtCtrls, StdCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    LabeledEdit1: TLabeledEdit;
+    LabeledEdit2: TLabeledEdit;
+    Process1: TProcessUTF8;
+    procedure Button1Click(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure LabeledEdit1Change(Sender: TObject);
+    procedure LabeledEdit1KeyPress(Sender: TObject; var Key: char);
+    procedure LabeledEdit2Change(Sender: TObject);
+    procedure LabeledEdit2KeyPress(Sender: TObject; var Key: char);
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end;
+
+var Form1: TForm1;
+function check_input(input:string):Boolean;
+procedure wipe_disk(passes:string;disk:string);
+procedure restrict_pass_input(var key:char);
+procedure restrict_drive_input(var key:char);
+procedure window_setup();
+procedure interface_setup();
+procedure language_setup();
+
+implementation
+
+function check_input(input:string):Boolean;
+var target:Boolean;
+begin
+target:=True;
+if input='' then
+begin
+target:=False;
+end;
+check_input:=target;
+end;
+
+procedure wipe_disk(passes:string;disk:string);
+var wiper:string;
+begin
+wiper:=ExtractFilePath(Application.ExeName)+'fastwiper';
+Form1.Process1.Executable:=wiper;
+Form1.Process1.Parameters.Add(passes);
+Form1.Process1.Parameters.Add(disk);
+Form1.Process1.Execute();
+end;
+
+procedure restrict_pass_input(var key:char);
+begin
+if not(key in ['0'..'9']) then
+begin
+if ord(key)<>VK_BACK then key:=#0;
+end;
+
+end;
+
+procedure restrict_drive_input(var key:char);
+begin
+if not(key in ['A'..'Z','a'..'z']) then
+begin
+if ord(key)<>VK_BACK then key:=#0;
+end;
+
+end;
+
+procedure window_setup();
+begin
+ Application.Title:='FAST WIPER SHELL';
+ Form1.Caption:='FAST WIPER SHELL 0.6';
+ Form1.BorderStyle:=bsDialog;
+ Form1.Font.Name:=Screen.MenuFont.Name;
+ Form1.Font.Size:=14;
+end;
+
+procedure interface_setup();
+begin
+Form1.Button1.Enabled:=False;
+Form1.LabeledEdit1.MaxLength:=3;
+Form1.LabeledEdit2.MaxLength:=1;
+Form1.LabeledEdit1.LabelPosition:=lpLeft;
+Form1.LabeledEdit2.LabelPosition:=lpLeft;
+Form1.LabeledEdit1.Text:='';
+Form1.LabeledEdit2.Text:='';
+end;
+
+procedure language_setup();
+begin
+Form1.Button1.Caption:='Wipe';
+Form1.LabeledEdit1.EditLabel.Caption:='Amount of wipe passes';;
+Form1.LabeledEdit2.EditLabel.Caption:='Drive letter';
+end;
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+window_setup();
+interface_setup();
+language_setup();
+end;
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+wipe_disk(Form1.LabeledEdit1.Text,Form1.LabeledEdit2.Text);
+end;
+
+procedure TForm1.LabeledEdit1Change(Sender: TObject);
+begin
+Form1.Button1.Enabled:=check_input(Form1.LabeledEdit1.Text) and check_input(Form1.LabeledEdit2.Text);
+end;
+
+procedure TForm1.LabeledEdit1KeyPress(Sender: TObject; var Key: char);
+begin
+restrict_pass_input(key);
+end;
+
+procedure TForm1.LabeledEdit2Change(Sender: TObject);
+begin
+Form1.Button1.Enabled:=check_input(Form1.LabeledEdit1.Text) and check_input(Form1.LabeledEdit2.Text);
+end;
+
+procedure TForm1.LabeledEdit2KeyPress(Sender: TObject; var Key: char);
+begin
+restrict_drive_input(key);
+end;
+
+end.
---
+++ readme.txt
@@ -0,0 +1,35 @@
+                              FAST WIPER SHELL
+
+                    This program was create by Popov Evgeniy Alekseyevich
+
+                             Version 0.6
+
+System requirement
+
+Windows 2000 or higher
+
+Program description
+
+This program is a graphic shell for FAST WIPER.
+Put executable file of FAST WIPER to FAST WIPER SHELL directory before first start.
+
+License
+
+This program distrubuted under GNU GENERAL PUBLIC LICENSE.
+
+Contact
+
+You can send me a letter to tuzik87@inbox.ru
+
+About program modification
+
+Source code is included in source.zip archive.
+It can be compiled under Lazarus 1.6 or higher.
+
+Change log
+
+0.1 - Initial version
+0.2 - 0.3 - Small changes
+0.4 - Small bug was fixed
+0.5 - Small changes
+0.6 - Small bug was fixed
